75024445f00dd3891785f9e8ba92a3b95208ef0d
HarCoreWare <bernardocodu@gmail.com>
2018-10-04 00:53:26 -0500

    light core

--- app/core/manager.php
+++ app/core/manager.php
@@ -1,9 +1,9 @@
 <?php
 
 //Manager Includes all classes that app needs and initializes the core
+
 require_once('../app/lib/crud.php');
 require_once('../app/config/config.php');
-require_once('core.php');
 require_once('router.php');
 
 $router = new router();
