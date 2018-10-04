69495989b172330f65d0b85b6fe2a5ba6494c14c
HarCoreWare <bernardocodu@gmail.com>
2018-10-04 00:42:36 -0500

    light core

--- app/models/outputclient.php
+++ app/models/outputclient.php
@@ -2,8 +2,12 @@
 
     require_once('output.php');
 
+    if(isset($_POST["req"])){
+
     $request=$_POST["req"];
 
     $output = new output($request,SQL_HOST,SQL_USER,SQL_PASSWORD,SQL_DATABASE);
 
+    }
+    
 ?>
